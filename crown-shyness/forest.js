c798cf117d82ae2e5b1aa02a66f1e23786dc6773
Radka Hoskova <hoskra@gmail.com>
2022-04-09 23:44:53 +0200

    cleanup and colors

--- crown-shyness/forest.js
+++ crown-shyness/forest.js
@@ -23,9 +23,9 @@ export class Forest {
     }
   }
 
-  drawTrees() {
+  drawTrees(color = true) {
     for (let tree of this.trees) {
-      tree.draw();
+      tree.draw(color);
     }
   }
 
@@ -41,133 +41,45 @@ export class Forest {
     }
   }
 }
-
-function getKey(x,y, polygonIndex) {
-  let strX = x.toString();
-  if(strX.length == 1) {
-    strX = "000" + strX;
-  } else if (strX.length == 2) {
-    strX = "00" + strX;
-  } else if (strX.length == 3) {
-    strX = "0" + strX;
-  }
-  let strY = y.toString();
-  if(strY.length == 1) {
-    strY = "000" + strY;
-  } else if (strY.length == 2) {
-    strY = "00" + strY;
-  } else if (strY.length == 3) {
-    strY = "0" + strY;
-  }
-  let polygonIndexStr = polygonIndex.toString();
-  if(polygonIndexStr.length == 1) { 
-    polygonIndexStr = "0" + polygonIndexStr;
-  }
-  return strX + strY + polygonIndexStr;
-}
-
-function getVal(key) {
-  return [parseInt(key.substring(0,4)), parseInt(key.substring(4,8)), parseInt(key.substring(8,10))];
-}
-
 export class Tree {
   constructor() {
     this.points   = [];
-    this.envelope = [];
     this.polygons = [];
     this.color = getRandomColor(); 
-    this.center;
-    this.envelopePoints = [];
     this.faces = [];
+    this.polygon;
   }
 
   createEnvelope() {
-    let tmp = new Map();
-    
-    for (let polygon of this.polygons) {
-      for (let point of polygon) {
-        let key = getKey(point[0], point[1], this.polygons.indexOf(polygon));
-        if(tmp.has(key)) {
-          tmp.set(key, tmp.get(key) + 1);
-        } else {
-          tmp.set(key, 1);
-        }
-      }
-      this.envelopePoints.push([]);
-    }
-
-    let avgX = 0;
-    let avgY = 0;
-    let cnt = 0;
-
-    // remove inside points
-    tmp.forEach((val, key) => {
-      if(val < 3) {
-        let x = getVal(key)[0];
-        let y = getVal(key)[1];
-        let polygonIndex = getVal(key)[2];
-        this.envelope.push([x,y]);
-        this.envelopePoints[polygonIndex].push([x,y]);
-        avgX += x;
-        avgY += y;
-        cnt++;
-      }
-    });
-
-    this.center = [avgX/cnt, avgY/cnt];
-    // this.envelope.forEach(s => circle(s[0], s[1], 30));
-
     let toPoly = [];
     this.polygons.forEach(polygon => {
-      toPoly.push( { regions: [polygon ], inverted: false} ) } );
-
+      toPoly.push( { regions: [polygon ], inverted: false} )
+    });
 
     
-  var segments = PolyBool.segments(toPoly[0]);
-  for (var i = 1; i < toPoly.length; i++){
-    var seg2 = PolyBool.segments(toPoly[i]);
-    var comb = PolyBool.combine(segments, seg2);
-    segments = PolyBool.selectUnion(comb);
+    var segments = PolyBool.segments(toPoly[0]);
+    for (var i = 1; i < toPoly.length; i++){
+      var seg2 = PolyBool.segments(toPoly[i]);
+      var comb = PolyBool.combine(segments, seg2);
+      segments = PolyBool.selectUnion(comb);
+    }
+
+    this.polygon = PolyBool.polygon(segments).regions[0];
+    
   }
-
-
+  
+  drawEnvelope() {
     beginShape();
-    let x = PolyBool.polygon(segments).regions[0];
-    console.log(x)
-
-    fill(this.color);
-    x.forEach(element => {
+    this.polygon.forEach(element => {
       vertex(element[0], element[1]); 
-      line(element[0], element[1], this.center[0], this.center[1]);
     });
     endShape(CLOSE);
   }
 
-  drawEnvelope() {
-
-    // this.polygons.forEach(polygon => {
-    //   polygon.forEach((p,i) => {
-    //     if (this.envelopePoints.includes(p)) {
-    //       line(polygon[i][0], polygon[i][1], 
-    //         polygon[(i+1)%polygon.length][0], 
-    //         polygon[(i+1)%polygon.length][1]);
-    //     }
-        
-    //   })
-    // });
-
-    // for (let i = 0; i < this.envelopePoints[0].length; i++) {
-    //   line(this.envelopePoints[0][i][0], this.envelopePoints[0][i][1], 
-    //     this.envelopePoints[0][(i+1)%this.envelopePoints[0].length][0], 
-    //     this.envelopePoints[0][(i+1)%this.envelopePoints[0].length][1]);
-    // }
-
-    circle(this.center[0], this.center[1], 10);
-  }
-
-  draw() {
+  draw(color = true) {
     this.polygons.forEach( (polygon, i) => {
-      fill(this.color);
+      if(color)
+        fill(this.color);
       beginShape();
       for (let p of polygon) {
         vertex(p[0], p[1]);
